a04e394fcc39375bc2598c4fbaa76555cb63325c
dmytro-dontsov <qemmnes@gmail.com>
2023-10-05 10:54:55 +0200

    parallel animation

--- src/main/java/com/example/project2dgames/HelloController.java
+++ src/main/java/com/example/project2dgames/HelloController.java
@@ -3,6 +3,7 @@ package com.example.project2dgames;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.Animation;
 import javafx.animation.Interpolator;
 import javafx.animation.ParallelTransition;
 import javafx.animation.TranslateTransition;
@@ -31,13 +32,15 @@ public class HelloController {
         bgOneTransition.setFromX(0);
         bgOneTransition.setToX(BG_WIDTH * -1);
         bgOneTransition.setInterpolator(Interpolator.LINEAR);
-        bgOneTransition.play();
 
         TranslateTransition bgTwoTransition = new TranslateTransition(Duration.millis(5000), bg2);
         bgTwoTransition.setFromX(0);
         bgTwoTransition.setToX(BG_WIDTH * -1);
         bgTwoTransition.setInterpolator(Interpolator.LINEAR);
-        bgTwoTransition.play();
+
+        parallelTransition = new ParallelTransition(bgOneTransition, bgTwoTransition);
+        parallelTransition.setCycleCount(Animation.INDEFINITE);
+        parallelTransition.play();
     }
 
 }
