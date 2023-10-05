e4c6a74b4b9ec6b2df3d035b71fdcca76c0f1e47
dmytro-dontsov <qemmnes@gmail.com>
2023-10-05 09:38:08 +0200

    basic sttings

--- src/main/java/com/example/project2dgames/HelloApplication.java
+++ src/main/java/com/example/project2dgames/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -11,8 +12,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 710, 399);
+        stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/project2dgames/HelloController.java
+++ src/main/java/com/example/project2dgames/HelloController.java
@@ -1,8 +1,27 @@
 package com.example.project2dgames;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ImageView bg1, bg2, player;
+
+    @FXML
+    void initialize() {
+        assert bg1 != null : "fx:id=\"bg1\" was not injected: check your FXML file 'hello-view.fxml'.";
+        assert bg2 != null : "fx:id=\"bg2\" was not injected: check your FXML file 'hello-view.fxml'.";
+        assert player != null : "fx:id=\"player\" was not injected: check your FXML file 'hello-view.fxml'.";
+
+    }
 
 }
